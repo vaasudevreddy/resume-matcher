435d72179edcf37f222f609b7c86c6f9df60b5e7
vaasudevreddy <nagamvasudevareddy@gmail.com>
2026-02-28 14:57:34 +0100

    Added Render port configuration

--- app.py
+++ app.py
@@ -96,5 +96,8 @@ def home():
     return render_template("index.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
