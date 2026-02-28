10125b1b718e4591a9302997c1756f9223a8d7b7
vaasudevreddy <nagamvasudevareddy@gmail.com>
2026-02-28 15:15:54 +0100

    Fixed gunicorn port binding

--- app.py
+++ app.py
@@ -94,10 +94,3 @@ def home():
         return render_template("ranking.html", results=results)
 
     return render_template("index.html")
-
-
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
